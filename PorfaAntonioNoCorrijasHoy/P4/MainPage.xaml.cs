04ac7c3b60b5289d97ada118acdc8d84269c7a04
Jose Daniel <jrave@ucm.es>
2021-04-29 16:24:11 +0200

    callate anda callate

---
+++ PorfaAntonioNoCorrijasHoy/P4/MainPage.xaml.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Core;
+using Windows.UI.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace P4
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public ObservableCollection<VMDron> ListaDrones { get; } = new ObservableCollection<VMDron>();
+        PointerPoint ptrPt;
+        int Sel = -1;
+        bool BotDer = false, BotIzq = false;
+
+        CoreCursor pin;
+        CoreCursor normal;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            pin = new CoreCursor(CoreCursorType.Pin, 0);
+            normal = new CoreCursor(CoreCursorType.Arrow, 0);
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // Cosntruye las listas de ModelView a partir de la lista Modelo 
+            if (ListaDrones != null)
+                foreach (Dron dron in Model.GetAllDrones())
+                {
+                    VMDron VMitem = new VMDron(dron);
+                    ListaDrones.Add(VMitem);
+                }
+            base.OnNavigatedTo(e);
+        }
+
+
+        private void ImageGridView_ItemClick(object sender, ItemClickEventArgs e) {
+            VMDron Item = e.ClickedItem as VMDron;
+            Sel = Item.Id;
+
+            //Declara la imagen que se ha seleccionado para mostrarla abajo a la derecha
+            Imagen.Source = Item.Img.Source;
+            //Pone el texto que describe a la imagen
+            Texto.Text = Item.Explicacion;
+            //Actualizamos la imagen que se va a poner encima del mapa
+            ImagenC.Content = Item.Img;
+
+            //ImagenC.
+            Canvas.SetLeft(ImagenC, Item.X);
+            Canvas.SetTop(ImagenC, Item.Y);
+
+            CompositeT.Rotation = 0;
+        }
+
+        private void ImagenC_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            PointerPoint NewptrPt = e.GetCurrentPoint(MiCanvas);
+            //float dX = (int)NewptrPt.Position.X - (int)ptrPt.Position.X;
+            if ((Sel >= 0) && ((BotIzq || BotDer)))
+            Window.Current.CoreWindow.PointerCursor = pin;
+            {
+                if (BotIzq) {
+
+                    Canvas.SetLeft(ImagenC, (int)NewptrPt.Position.X - 25);
+                    Canvas.SetTop(ImagenC, (int)NewptrPt.Position.Y - 25);
+                    //ListaDrones[Sel].Transform.TranslateX = (int)NewptrPt.Position.X - 25;
+                    //ListaDrones[Sel].Transform.TranslateY = (int)NewptrPt.Position.Y - 25;
+                }
+
+                if (BotDer)
+                {
+                    ListaDrones[Sel].Angulo = (int)NewptrPt.Position.X - (int)ptrPt.Position.X;
+                    ListaDrones[Sel].Transform.Rotation = ListaDrones[Sel].Angulo;
+
+                    ImagenC.RenderTransform = ListaDrones[Sel].Transform;
+
+                    
+                }
+
+                //ListaDrones[Sel].Transform.TranslateY = ListaDrones[Sel].Y - 25;
+                //ListaDrones[Sel].Transform.TranslateX = ListaDrones[Sel].X - 25;
+
+                //ListaDrones[Sel].Transform.CenterX = 25;
+                //ListaDrones[Sel].Transform.CenterY = 25;
+
+            }
+
+        }
+
+        private void ImagenC_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            PointerPoint ptrPt = e.GetCurrentPoint(MiCanvas);
+            if (!ptrPt.Properties.IsLeftButtonPressed) BotIzq = false;
+            if (!ptrPt.Properties.IsRightButtonPressed) BotDer = false;
+
+            Window.Current.CoreWindow.PointerCursor = normal;
+        }
+
+        private void CommandBar_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if(e.Key == Windows.System.VirtualKey.Tab){
+                e.Handled = true;
+                DependencyObject candidate = null;
+                candidate = FocusManager.FindNextFocusableElement(FocusNavigationDirection.Down);
+                (candidate as Control).Focus(FocusState.Keyboard);
+            }
+        }
+
+        private void ImageC_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            bool move = false;
+            if (Sel >= 0){
+                //int X = ListaDrones[Sel].X;
+                //int Y = ListaDrones[Sel].Y;
+
+                int X = (int)Canvas.GetLeft(ImagenC);
+                int Y = (int)Canvas.GetTop(ImagenC);
+
+                int Angulo = ListaDrones[Sel].Angulo;
+
+
+                switch (e.Key)
+                {
+                    //Izquierda
+                    case VirtualKey.A:
+                    case VirtualKey.GamepadRightThumbstickLeft:
+                        X -= 10;
+                        move = true;
+                        break;
+                        //Derecha
+                    case VirtualKey.D:
+                    case VirtualKey.GamepadRightThumbstickRight:
+                        X += 10;
+                        move = true;
+                        break;
+                        //Arriba
+                    case VirtualKey.W:
+                    case VirtualKey.GamepadRightThumbstickUp:
+                        Y -=  10;
+                        move = true;
+                        break;
+                    case VirtualKey.S:
+                    case VirtualKey.GamepadRightThumbstickDown:
+                        Y += 10;
+                        move = true;
+                        break;
+                    case VirtualKey.Q:
+                    case VirtualKey.GamepadX:
+                        Angulo--;
+                        move = true;
+                        break;
+                    case VirtualKey.E:
+                    case VirtualKey.GamepadY:
+                        Angulo++;
+                        move = true;
+                        break;
+                        //Abajo
+                }
+
+
+                if(move){
+                    Canvas.SetLeft(ImagenC, (int)X);
+                    Canvas.SetTop(ImagenC, (int)Y);
+
+                    
+                    ListaDrones[Sel].Angulo = Angulo;
+                    CompositeTransform transformation = new CompositeTransform();
+                    transformation.Rotation = Angulo;
+                    transformation.CenterX = 25;
+                    transformation.CenterY = 25;
+                    ImagenC.RenderTransform = transformation;
+                }
+            }
+        }
+
+        private void ImagenC_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            ptrPt = e.GetCurrentPoint(MiCanvas);
+            if (ptrPt.Properties.IsLeftButtonPressed) BotIzq = true;
+            //Establecer Cursor
+            if (ptrPt.Properties.IsRightButtonPressed) BotDer = true;
+
+
+        }
+    }
+}
---
+++ PorfaAntonioNoCorrijasHoy/P4/ViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace P4
+{
+    public class VMDron : Dron
+    {
+        public Image Img;
+        public ContentControl CCImg;
+        public int Zoom;
+        public int Angulo;
+        public CompositeTransform Transform;
+
+        public VMDron(Dron dron)
+            {
+            Id = dron.Id;
+            Nombre = dron.Nombre;
+            Imagen = dron.Imagen;
+            Explicacion = dron.Explicacion;
+            Estado = dron.Estado;
+            X = dron.X;
+            Y = dron.Y;
+            RX = dron.RX;
+            RY = dron.RY;
+            Img = new Image();
+            string s = System.IO.Directory.GetCurrentDirectory() + "\\" + dron.Imagen;
+            Img.Source = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri(s));
+            Img.Width = 50;
+            Img.Height = 50;
+
+            //Lo comento aquí porque lo creo en el Image ItemClick y da conflicto si asigno los dos a la misma imagen
+            //CCImg = new ContentControl();
+            //CCImg.Content = Img;
+            //CCImg.UseSystemFocusVisuals = true;
+
+            Angulo = 0;
+            Transform = new CompositeTransform();
+            Transform.Rotation = Angulo;
+
+            //No hacer esto
+            //Transform.TranslateX = X - 25;
+            //Transform.TranslateY = Y - 25;
+            Transform.CenterX = 25;
+            Transform.CenterY = 25;
+
+
+            //CCImg.Visibility = Windows.UI.Xaml.Visibility.Visible;//.Collapsed;
+            //Mapa.Children.Add(CCImg);
+            //Mapa.Children.Last().SetValue(Canvas.LeftProperty, X - 25);
+            //Mapa.Children.Last().SetValue(Canvas.TopProperty, Y - 25);
+        }
+    }
+
+
+   
+}
